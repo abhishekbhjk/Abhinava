5fc8dd452bd86f6253af419e1927974852e7f239
Abhishek b h <abhishekbh0606@gmail.com>
2024-09-19 20:29:55 +0530

    Update day2.v

--- week1/day2.v
+++ week1/day2.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
-module mem(clk,rst,wr,rd,,address,Datain,Dataout);
+module mem(clk,rst,wr,rd,,address,datain,dataout);
 input clk,rst,wr,rd;
-input [7:0]Datain;
+  input [7:0]datain;
 input [3:0]address;
 reg [7:0] memory[0:10];
-output reg[7:0]Dataout;
+output reg[7:0]dataout;
 always@(posedge clk)
 begin
 if(rst)
@@ -14,9 +14,9 @@ end
 else
 begin
 case({wr,rd})
-2'b10:memory[address]=Datain;
-2'b01:Dataout=memory[address];
-default:Dataout=8'bz;
+2'b10:memory[address]=datain;
+2'b01:dataout=memory[address];
+default:dataout=8'bz;
 endcase
 end
 end
